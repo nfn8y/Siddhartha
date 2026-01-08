1dec6820a7753de742a3eb0f7c80bd628754ddf2
Sukanya15 <sukanya18109@iiitd.ac.in>
2026-01-08 19:51:43 +0100

    Initial Commit

---
+++ SiddharthaTests/SiddharthaTests.swift
@@ -0,0 +1,16 @@
+//
+//  SiddharthaTests.swift
+//  SiddharthaTests
+//
+//  Created by Kumar Yashwant on 1/8/26.
+//
+
+import Testing
+
+struct SiddharthaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
